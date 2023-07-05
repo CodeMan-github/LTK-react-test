fe61d4be840c265f544b76a37541b22c8c94a91d
codeman <top.talented@outlook.com>
2023-07-05 10:59:10 -0600

    feat: add todo table and fix styles

--- src/scenes/BasicForm.js
+++ src/scenes/BasicForm.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Formik, Field, Form } from 'formik';
-import { Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import {
+  Table,
+  Button,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TextField,
+  TableHead,
+  Paper,
+} from '@mui/material';
+import { addTodo, removeTodo } from '../state';
 
-const BasicForm = () => (
-  <div>
-    <h1>TODO</h1>
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={(values) => {}}
-    >
-      <Form>
-        <label htmlFor="todo">Add ToDo </label>
-        <Field id="todo" name="Add ToDo" placeholder="TextHere" />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
-  </div>
-);
+const validationSchema = yup.object({
+  todo: yup
+    .string('Enter todo')
+    .required('Todo is required'),
+});
+
+const BasicForm = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const formik = useFormik({
+    initialValues: {
+      todo: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      const _id = Date.now();
+      const todo = { _id, name: values.todo };
+      dispatch(addTodo(todo));
+    },
+  });
+
+  return (
+    <div>
+      <h1>TODO</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="todo"
+          name="todo"
+          label="Todo"
+          value={formik.values.todo}
+          onChange={formik.handleChange}
+          error={formik.touched.todo && Boolean(formik.errors.todo)}
+          helperText={formik.touched.todo && formik.errors.todo}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {todos.map((todo) => (
+              <TableRow
+                key={todo._id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {todo._id}
+                </TableCell>
+                <TableCell align="right">{todo.name}</TableCell>
+                <TableCell align="right">
+                  <Button onClick={() => dispatch(removeTodo(todo))}>
+                    Remove
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
 
 export default BasicForm
