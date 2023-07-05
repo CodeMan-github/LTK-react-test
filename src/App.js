847f86e645bc0bfcf3a7dfb448715eb1f281701f
codeman <top.talented@outlook.com>
2023-07-06 04:29:48 +0800

    feat: add home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import AccountMenu from './components/AccountMenu';
-import BasicForm from './scenes/BasicForm'
+import BasicForm from './scenes/BasicForm';
+import Home from './scenes/Home';
 
 export default function App() {
 	const navigate = useNavigate();
@@ -15,13 +14,9 @@ export default function App() {
     <Container maxWidth="sm">
 			<AccountMenu onClick={handleOnClick} />
 			<Routes>
+				<Route path="/" element={<Home />} />
 				<Route path="/todo" element={<BasicForm />} />
 			</Routes>
-			<Box sx={{ my: 4 }}>
-				<Typography variant="h4" component="h1" gutterBottom>
-					React Skills test
-				</Typography>
-			</Box>
     </Container>
   );
 }
